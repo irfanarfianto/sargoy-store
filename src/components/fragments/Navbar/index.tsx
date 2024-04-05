3b1d780ed9958d746397f06f29df23a005585a61
irfanarfianto <irfanarfianto92@gmail.com>
2024-04-06 00:01:26 +0700

    feat: update navbar

--- src/components/fragments/Navbar/index.tsx
+++ src/components/fragments/Navbar/index.tsx
@@ -1,20 +1,14 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from './Navbar.module.scss'
 
+
 const Navbar = () => {
-   const { data: session } = useSession(); // Menyimpan objek sesi ke dalam variabel session
-
-   // Menampilkan nama lengkap pengguna jika sesi tersedia
-   const userFullName = session?.user?.fullname || "Guest";
-
+   const { data } = useSession();
    return (
       <div className={styles.navbar}>
-         {/* Menampilkan tombol login/logout */}
-         <button className={styles.navbar__button} onClick={() => session ? signOut() : signIn()}>
-            {session ? 'Logout' : 'Login'}
-         </button>
-         {/* Menampilkan nama lengkap pengguna jika tersedia */}
-         {session && <span className={styles.navbar__user}>{userFullName}</span>}
+         <button className={styles.navbar__button} onClick={() => data ? signOut() : signIn()}>
+         {data ? 'Logout' : 'Login'}
+      </button>
       </div>
    )
 }
